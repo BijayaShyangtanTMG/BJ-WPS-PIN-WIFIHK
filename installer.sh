f1afe27cd0a5213e098de58d27bfd6eccfb1fa1f
BijayaShyangtanTMG <bijaya.tamang.shyangtan24@gmail.com>
2024-08-06 15:06:51 +0545

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,22 @@
+#!/data/data/com.termux/files/usr/bin/bash
+pkg install -y root-repo 
+pkg install -y git tsu python wpa-supplicant pixiewps iw
+
+git clone https://github.com/W8SOJIB/W8RootWifiHK
+
+chmod +x BJ-WPS-PIN-WIFIHK/BJ-wps.py
+
+printf "
+
+
+██████╗      ██╗    ████████╗███╗   ███╗ ██████╗ 
+██╔══██╗     ██║    ╚══██╔══╝████╗ ████║██╔════╝ 
+██████╔╝     ██║       ██║   ██╔████╔██║██║  ███╗
+██╔══██╗██   ██║       ██║   ██║╚██╔╝██║██║   ██║
+██████╔╝╚█████╔╝       ██║   ██║ ╚═╝ ██║╚██████╔╝
+╚═════╝  ╚════╝        ╚═╝   ╚═╝     ╚═╝ ╚═════╝ 
+                                                 
+_________________©Developed by BIJAYA TAMANG___________________
+
+
+###############################################\n#  All done! Now you can run script with\n#   python3 BJ-wps.py -i wlan0 -K\n#\n#  Thanks for using my project ©BJ,\n#\n###############################################\n"
